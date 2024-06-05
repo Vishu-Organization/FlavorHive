a47085dcd753d3cffe8c0ba127f4887a28ae0e10
Vishu Kumar <1992vishukumar@gmail.com>
2024-06-05 16:13:21 +0530

    vite config updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "src/unit-tests/setup.ts",
+    dir: "src/unit-tests",
+    reporters: "default",
+    coverage: {
+      enabled: true,
+      reporter: ["html"],
+      reportsDirectory: "src/unit-tests/coverage",
+      reportOnFailure: true,
+      thresholds: {
+        lines: 60,
+        branches: 60,
+        functions: 60,
+        statements: 80,
+      },
+    },
+  },
+});
