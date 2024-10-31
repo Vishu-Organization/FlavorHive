d22396f4bad8cdbf2d30635f51549d145970f2a5
Vishu Kumar <1992vishukumar@gmail.com>
2024-10-31 12:39:54 +0530

    Added whatscooking to Login and ourvision

--- src/components/company-pages/OurVision.tsx
+++ src/components/company-pages/OurVision.tsx
@@ -1,6 +1,7 @@
 import { createLazyRoute, useLoaderData } from "@tanstack/react-router";
 import OurVisionImages from "./OurVisionImages";
 import { useEffect, useState } from "react";
+import WhatsCooking from "../Discover/WhatsCooking";
 
 const OurVision = () => {
   const ourVision = useLoaderData({ from: "/pages/vision" });
@@ -19,37 +20,40 @@ const OurVision = () => {
   }, []);
 
   return (
-    <div className="container grid space-y-10 text-center">
-      <iframe
-        data-testid="youtube"
-        style={{
-          height: `${iframeHeight < 550 ? iframeHeight : 550}px`,
-        }}
-        width="100%"
-        src="https://www.youtube.com/embed/C_Xgn87CF-I"
-        title="YouTube video player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      ></iframe>
-      <p
-        data-testid="main-vision"
-        className="font-chronicle text-balance text-base leading-8 text-primary lg:text-[20px]"
-      >
-        {ourVision && ourVision[0].description}
-      </p>
-      <div className="relative h-96 w-full bg-our-vision bg-cover bg-center bg-no-repeat text-2xl transition-all duration-200 ease-in hover:scale-[1.01] md:h-[500px] md:text-4xl lg:h-screen lg:text-5xl">
-        <div className="absolute inset-0 bg-black opacity-25"></div>
-        <div className="relative top-1/2 z-10 mx-auto w-3/4 -translate-y-3/4 p-4 font-bold leading-tight text-white">
-          We're developing better standards for higher quality ingredients.
+    <>
+      <div className="container grid space-y-10 text-center">
+        <iframe
+          data-testid="youtube"
+          style={{
+            height: `${iframeHeight < 550 ? iframeHeight : 550}px`,
+          }}
+          width="100%"
+          src="https://www.youtube.com/embed/C_Xgn87CF-I"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        ></iframe>
+        <p
+          data-testid="main-vision"
+          className="text-balance font-chronicle text-base leading-8 text-primary lg:text-[20px]"
+        >
+          {ourVision && ourVision[0].description}
+        </p>
+        <div className="relative h-96 w-full bg-our-vision bg-cover bg-center bg-no-repeat text-2xl transition-all duration-200 ease-in hover:scale-[1.01] md:h-[500px] md:text-4xl lg:h-screen lg:text-5xl">
+          <div className="absolute inset-0 bg-black opacity-25"></div>
+          <div className="relative top-1/2 z-10 mx-auto w-3/4 -translate-y-3/4 p-4 font-bold leading-tight text-white">
+            We're developing better standards for higher quality ingredients.
+          </div>
         </div>
+        <div
+          data-testid="sub-vision"
+          className="mx-auto w-5/6 text-base text-primary-black lg:text-lg"
+        >
+          {ourVision && ourVision[1].description}
+        </div>
+        <OurVisionImages />
       </div>
-      <div
-        data-testid="sub-vision"
-        className="mx-auto w-5/6 text-base text-primary-black lg:text-lg"
-      >
-        {ourVision && ourVision[1].description}
-      </div>
-      <OurVisionImages />
-    </div>
+      <WhatsCooking />
+    </>
   );
 };
 
--- src/components/users/LogIn.tsx
+++ src/components/users/LogIn.tsx
@@ -9,6 +9,7 @@ import {
   useSignInWithPassword,
 } from "../../services/use-mutations";
 import Button from "../Button";
+import WhatsCooking from "../Discover/WhatsCooking";
 
 const LogIn = () => {
   const { mutate: signInWithGoogleMutate } = useSignInWithGoogle();
@@ -40,156 +41,159 @@ const LogIn = () => {
   });
 
   return (
-    <div className="container p-8">
-      <div className="mx-auto w-[65%] space-y-4 bg-white p-5 shadow-md md:w-[50%] lg:w-[40%] xl:w-[25%]">
-        <h1
-          className="mb-2 text-center text-xl font-bold text-primary md:text-2xl lg:text-3xl"
-          data-testid="text-login-header"
-        >
-          Login
-        </h1>
-        <form
-          className="space-y-4"
-          onSubmit={(e) => {
-            e.preventDefault();
-            handleSubmit();
-          }}
-        >
-          <Field
-            name="email"
-            validators={{
-              onChange: z
-                .string()
-                .min(1, "Email is required")
-                .email("Invalid email address"),
+    <>
+      <div className="container p-8">
+        <div className="mx-auto w-[65%] space-y-4 bg-white p-5 shadow-md md:w-[50%] lg:w-[40%] xl:w-[25%]">
+          <h1
+            className="mb-2 text-center text-xl font-bold text-primary md:text-2xl lg:text-3xl"
+            data-testid="text-login-header"
+          >
+            Login
+          </h1>
+          <form
+            className="space-y-4"
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSubmit();
             }}
-            children={({ state, handleChange, handleBlur }) => (
-              <TextField
-                id="email"
-                size="small"
-                label="Email"
-                variant="outlined"
-                defaultValue={state.value}
-                onChange={(e) => handleChange(e.target.value)}
-                onBlur={handleBlur}
-                placeholder="Enter email"
-                margin="dense"
-                fullWidth
-                error={!!state.meta.errors?.length}
-                helperText={
-                  state.meta.errors[0]?.toString().split(",").length === 1
-                    ? state.meta.errors
-                    : state.meta.errors[0]?.toString().split(",")[0]
-                }
-              />
-            )}
-          />
-          <Field
-            name="password"
-            validators={{
-              onChange: z
-                .string()
-                .min(1, "Password is required")
-                .min(6, "Password should be minimum 6 characters"),
-            }}
-            children={({ state, handleChange, handleBlur }) => (
-              <TextField
-                id="password"
-                type="password"
-                size="small"
-                label="Password"
-                variant="outlined"
-                defaultValue={state.value}
-                onChange={(e) => handleChange(e.target.value)}
-                onBlur={handleBlur}
-                placeholder="Enter password"
-                margin="normal"
-                fullWidth
-                error={!!state.meta.errors?.length}
-                helperText={
-                  state.meta.errors[0]?.toString().split(",").length === 1
-                    ? state.meta.errors
-                    : state.meta.errors[0]?.toString().split(",")[0]
-                }
-              />
-            )}
-          />
-          <div className="grid grid-cols-2 items-center text-[14px]">
+          >
             <Field
-              name="rememberMe"
-              children={({ state, handleChange }) => (
-                <div className="lets flex items-center space-x-2 text-primary-toggle-text">
-                  <input
-                    className="size-4"
-                    type="checkbox"
-                    id="remember"
-                    name="Remember Me?"
-                    checked={state.value}
-                    onChange={(e) => handleChange(e.target.checked)}
-                  />
-                  <label htmlFor="remember" className="hover:cursor-pointer">
-                    Remember Me?
-                  </label>
+              name="email"
+              validators={{
+                onChange: z
+                  .string()
+                  .min(1, "Email is required")
+                  .email("Invalid email address"),
+              }}
+              children={({ state, handleChange, handleBlur }) => (
+                <TextField
+                  id="email"
+                  size="small"
+                  label="Email"
+                  variant="outlined"
+                  defaultValue={state.value}
+                  onChange={(e) => handleChange(e.target.value)}
+                  onBlur={handleBlur}
+                  placeholder="Enter email"
+                  margin="dense"
+                  fullWidth
+                  error={!!state.meta.errors?.length}
+                  helperText={
+                    state.meta.errors[0]?.toString().split(",").length === 1
+                      ? state.meta.errors
+                      : state.meta.errors[0]?.toString().split(",")[0]
+                  }
+                />
+              )}
+            />
+            <Field
+              name="password"
+              validators={{
+                onChange: z
+                  .string()
+                  .min(1, "Password is required")
+                  .min(6, "Password should be minimum 6 characters"),
+              }}
+              children={({ state, handleChange, handleBlur }) => (
+                <TextField
+                  id="password"
+                  type="password"
+                  size="small"
+                  label="Password"
+                  variant="outlined"
+                  defaultValue={state.value}
+                  onChange={(e) => handleChange(e.target.value)}
+                  onBlur={handleBlur}
+                  placeholder="Enter password"
+                  margin="normal"
+                  fullWidth
+                  error={!!state.meta.errors?.length}
+                  helperText={
+                    state.meta.errors[0]?.toString().split(",").length === 1
+                      ? state.meta.errors
+                      : state.meta.errors[0]?.toString().split(",")[0]
+                  }
+                />
+              )}
+            />
+            <div className="grid grid-cols-2 items-center text-[14px]">
+              <Field
+                name="rememberMe"
+                children={({ state, handleChange }) => (
+                  <div className="lets flex items-center space-x-2 text-primary-toggle-text">
+                    <input
+                      className="size-4"
+                      type="checkbox"
+                      id="remember"
+                      name="Remember Me?"
+                      checked={state.value}
+                      onChange={(e) => handleChange(e.target.checked)}
+                    />
+                    <label htmlFor="remember" className="hover:cursor-pointer">
+                      Remember Me?
+                    </label>
+                  </div>
+                )}
+              />
+              <Link
+                data-testid="link-forgot-password"
+                className="justify-self-end"
+              >
+                Forgot Password?
+              </Link>
+            </div>
+            <Subscribe
+              selector={(state) => [
+                state.values.email && state.values.password,
+                state.canSubmit,
+                state.isSubmitting,
+                state.isTouched,
+              ]}
+              children={() => (
+                <div className="space-y-2 lg:space-y-4">
+                  <Button
+                    data-testid="btn-login"
+                    variant="default"
+                    type="submit"
+                    size="full"
+                    className="rounded-md bg-blue60"
+                  >
+                    Log In
+                  </Button>
+                  <div className="text-center text-sm font-medium">OR</div>
+                  <Button
+                    data-testid="btn-google"
+                    size="full"
+                    display="flex"
+                    variant="google"
+                    className="rounded-md"
+                    onClick={onSignInWithGoogle}
+                  >
+                    <GoogleIcon />
+                    <span className="text-sm font-medium normal-case">
+                      Sign in with Google
+                    </span>
+                  </Button>
                 </div>
               )}
             />
+          </form>
+          <h2 className="text-center text-sm">
+            <span className="text-primary-info">
+              Don't have an account?&nbsp;
+            </span>
             <Link
-              data-testid="link-forgot-password"
-              className="justify-self-end"
+              data-testid="link-signup"
+              className="font-extrabold"
+              to={"/users/sign-up"}
             >
-              Forgot Password?
+              Sign Up
             </Link>
-          </div>
-          <Subscribe
-            selector={(state) => [
-              state.values.email && state.values.password,
-              state.canSubmit,
-              state.isSubmitting,
-              state.isTouched,
-            ]}
-            children={() => (
-              <div className="space-y-2 lg:space-y-4">
-                <Button
-                  data-testid="btn-login"
-                  variant="default"
-                  type="submit"
-                  size="full"
-                  className="bg-blue60 rounded-md"
-                >
-                  Log In
-                </Button>
-                <div className="text-center text-sm font-medium">OR</div>
-                <Button
-                  data-testid="btn-google"
-                  size="full"
-                  display="flex"
-                  variant="google"
-                  className="rounded-md"
-                  onClick={onSignInWithGoogle}
-                >
-                  <GoogleIcon />
-                  <span className="text-sm font-medium normal-case">
-                    Sign in with Google
-                  </span>
-                </Button>
-              </div>
-            )}
-          />
-        </form>
-        <h2 className="text-center text-sm">
-          <span className="text-primary-info">
-            Don't have an account?&nbsp;
-          </span>
-          <Link
-            data-testid="link-signup"
-            className="font-extrabold"
-            to={"/users/sign-up"}
-          >
-            Sign Up
-          </Link>
-        </h2>
+          </h2>
+        </div>
       </div>
-    </div>
+      <WhatsCooking />
+    </>
   );
 };
 
