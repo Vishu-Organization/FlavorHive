daa66f01c2f0e57cd3ecb2b32f76f8a66c2b8c10
Vishu Kumar <1992vishukumar@gmail.com>
2024-10-31 12:44:47 +0530

    added tests for whatscooking in login screen

--- e2e/fixtures/login.ts
+++ e2e/fixtures/login.ts
@@ -3,15 +3,19 @@ import { LoginPage } from "../page-objects/login-page";
 import { SignupPage } from "../page-objects/signup-page";
 import { RootPage } from "../page-objects/root-page";
 import { HeaderPage } from "../page-objects/header-page";
+import { WhatsCookingPage } from "../page-objects/WhatsCooking/whats-cooking-page";
 
 export const loginTest = base.extend<{
   loginPage: LoginPage;
   signupPage: SignupPage;
   rootPage: RootPage;
   headerPage: HeaderPage;
+  whatsCookingPage: WhatsCookingPage;
 }>({
   loginPage: async ({ page }, use) => await use(new LoginPage(page)),
   signupPage: async ({ page }, use) => await use(new SignupPage(page)),
   rootPage: async ({ page }, use) => await use(new RootPage(page)),
   headerPage: async ({ page }, use) => await use(new HeaderPage(page)),
+  whatsCookingPage: async ({ page }, use) =>
+    await use(new WhatsCookingPage(page)),
 });
